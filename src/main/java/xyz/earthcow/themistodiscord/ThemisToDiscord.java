149cee516fa47d4c12bd359f99f4cdcff0395fd2
EarthCow <coppergriffing@gmail.com>
2023-12-18 16:51:54 -0500

    Initial commit

---
+++ src/main/java/xyz/earthcow/themistodiscord/ThemisToDiscord.java
@@ -0,0 +1,16 @@
+package xyz.earthcow.themistodiscord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThemisToDiscord extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
