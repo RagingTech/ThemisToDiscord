76eb66870b4d6c81dc767bd1a70c0e028fbe15e6
EarthCow <coppergriffing@gmail.com>
2023-12-18 19:17:33 -0500

    Command, config, and webhook functionality

--- src/main/java/xyz/earthcow/themistodiscord/ThemisToDiscord.java
+++ src/main/java/xyz/earthcow/themistodiscord/ThemisToDiscord.java
@@ -1,16 +1,78 @@
 package xyz.earthcow.themistodiscord;
 
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.WebhookClientBuilder;
+import club.minnced.discord.webhook.exception.HttpException;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public final class ThemisToDiscord extends JavaPlugin {
+    public static ThemisToDiscord instance;
+    public static WebhookClient client;
 
     @Override
     public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        WebhookClient.setDefaultErrorHandler((client, message, throwable) -> {
+            getLogger().severe(String.format("[%s] %s%n", client.getId(), message));
+
+            if (throwable instanceof HttpException ex && ex.getCode() == 404) {
+                client.close();
+                return;
+            }
+
+            if (throwable != null)
+                throwable.printStackTrace();
+        });
+
+        TtdCommand ttdCommand = new TtdCommand();
+        Objects.requireNonNull(getCommand("ttd")).setExecutor(ttdCommand);
+        Objects.requireNonNull(getCommand("ttd")).setTabCompleter(ttdCommand);
+
+        String webhookUrl = getConfig().getString("webhookUrl");
+
+        if (isInvalidWebhookUrl(webhookUrl)) {
+            getLogger().warning("Webhook url is missing or invalid! Set one using /ttd url <url>");
+            return;
+        }
+
+        initializeWebhook(webhookUrl);
 
     }
 
     @Override
     public void onDisable() {
+        if (client != null && !client.isShutdown()) {
+            client.close();
+        }
+    }
 
+    public static boolean isInvalidWebhookUrl(@Nullable String url) {
+        if (url == null) return true;
+        return !WebhookClientBuilder.WEBHOOK_PATTERN.matcher(url).matches();
+    }
+
+    public static void initializeWebhook(String webhookUrl) {
+        if (isInvalidWebhookUrl(webhookUrl)) return;
+
+        WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
+        builder.setThreadFactory((job) -> {
+            Thread thread = new Thread(job);
+            thread.setName("ThemisWebhookThread");
+            thread.setDaemon(true);
+            return thread;
+        });
+        builder.setWait(true);
+
+        if (client != null) {
+            client.close();
+        }
+
+        client = builder.build();
     }
 }
---
+++ src/main/java/xyz/earthcow/themistodiscord/TtdCommand.java
@@ -0,0 +1,75 @@
+package xyz.earthcow.themistodiscord;
+
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.send.WebhookEmbed;
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TtdCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String cmd, @NotNull String[] args) {
+        if (args.length < 1) return false;
+
+        switch (args[0]) {
+            case "url" -> {
+                if (args.length < 2) {
+                    sender.sendMessage(ChatColor.RED + "/ttd url <url>");
+                    return true;
+                }
+                String webhookUrl = args[1];
+                if (ThemisToDiscord.isInvalidWebhookUrl(webhookUrl)) {
+                    sender.sendMessage(ChatColor.RED + "That is not a valid webhook url!");
+                    return true;
+                }
+                ThemisToDiscord.instance.getConfig().set("webhookUrl", webhookUrl);
+                ThemisToDiscord.instance.saveConfig();
+                ThemisToDiscord.initializeWebhook(args[1]);
+            }
+            case "test" -> {
+                WebhookClient client = ThemisToDiscord.client;
+                if (client == null || client.isShutdown()) {
+                    sender.sendMessage(ChatColor.RED + "Client is not initialized!");
+                    return true;
+                }
+                WebhookEmbed embed = new WebhookEmbedBuilder()
+                        .setColor(0x00FF00)
+                        .setAuthor(new WebhookEmbed.EmbedAuthor(sender.getName(), null, null))
+                        .setTitle(new WebhookEmbed.EmbedTitle("ThemisToDiscord Test", null))
+                        .setDescription("Testing the ThemisToDiscord webhook functionality")
+                        .build();
+
+                sender.sendMessage(ChatColor.AQUA + "Sending test message...");
+                client.send(embed).whenComplete((readonlyMessage, throwable) -> {
+                    if (readonlyMessage == null || throwable != null) {
+                        sender.sendMessage(ChatColor.RED + "Message failed to send.");
+                        return;
+                    }
+                    sender.sendMessage(ChatColor.GREEN + "Message was sent. Id: " + readonlyMessage.getId());
+                });
+            }
+            default -> {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String cmd, @NotNull String[] args) {
+        if (args.length == 0) {
+            return Arrays.asList("url", "test");
+        }
+        return null;
+    }
+}
