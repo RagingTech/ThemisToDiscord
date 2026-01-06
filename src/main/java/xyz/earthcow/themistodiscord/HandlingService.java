74428e859c90a6bf27804ddac70e20c2261c913b
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-01-06 18:04:20 -0500

    Uses Google Guava Cache to remove inactive records

--- src/main/java/xyz/earthcow/themistodiscord/HandlingService.java
+++ src/main/java/xyz/earthcow/themistodiscord/HandlingService.java
@@ -1,18 +1,19 @@
 package xyz.earthcow.themistodiscord;
 
 import com.gmail.olexorus.themis.api.CheckType;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import dev.dejvokep.boostedyaml.block.implementation.Section;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class HandlingService {
     private record CheckKey(UUID uuid, CheckType checkType) {}
     private record CheckData(long lastSent, int repetitionCount) {}
 
-    private final Map<CheckKey, CheckData> playerCheckData = new HashMap<>();
+    private final Cache<CheckKey, CheckData> playerCheckData;
 
     private final double executionThreshold;
     private final double repetitionDelay;
@@ -25,6 +26,10 @@ public class HandlingService {
         this.executionThreshold = handlingSection.getDouble("execution-threshold");
         this.repetitionDelay = handlingSection.getDouble("repetition-delay");
         this.repetitionThreshold = handlingSection.getDouble("repetition-threshold");
+
+        this.playerCheckData = CacheBuilder.newBuilder()
+                .expireAfterAccess(24, TimeUnit.HOURS)
+                .build();
     }
 
     public double getExecutionThreshold() {
@@ -40,14 +45,14 @@ public class HandlingService {
     }
 
     public long getLastSentTimeForPlayer(Player player, CheckType checkType) {
-        CheckData checkData = playerCheckData.get(new CheckKey(player.getUniqueId(), checkType));
+        CheckData checkData = playerCheckData.getIfPresent(new CheckKey(player.getUniqueId(), checkType));
         if (checkData == null) return DEFAULT_LAST_SENT;
         return checkData.lastSent;
     }
 
     public void updateLastSentTimeForPlayer(Player player, CheckType checkType) {
         CheckKey playerCheckDataKey = new CheckKey(player.getUniqueId(), checkType);
-        CheckData checkData = playerCheckData.get(playerCheckDataKey);
+        CheckData checkData = playerCheckData.getIfPresent(playerCheckDataKey);
         if (checkData == null) {
             checkData = new CheckData(System.currentTimeMillis(), DEFAULT_REPETITION_COUNT);
         } else {
@@ -57,14 +62,14 @@ public class HandlingService {
     }
 
     public int getRepetitionCountForPlayer(Player player, CheckType checkType) {
-        CheckData checkData = playerCheckData.get(new CheckKey(player.getUniqueId(), checkType));
+        CheckData checkData = playerCheckData.getIfPresent(new CheckKey(player.getUniqueId(), checkType));
         if (checkData == null) return DEFAULT_REPETITION_COUNT;
         return checkData.repetitionCount;
     }
 
     public void putRepetitionCountForPlayer(Player player, CheckType checkType, int repetitionCount) {
         CheckKey playerCheckDataKey = new CheckKey(player.getUniqueId(), checkType);
-        CheckData checkData = playerCheckData.get(playerCheckDataKey);
+        CheckData checkData = playerCheckData.getIfPresent(playerCheckDataKey);
         if (checkData == null) {
             checkData = new CheckData(DEFAULT_LAST_SENT, repetitionCount);
         } else {
