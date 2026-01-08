9b90cb958bb7c115ce8a35997b020fcd65cc5660
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-01-08 13:03:30 -0500

    Lessens Message method complexity

--- src/main/java/xyz/earthcow/themistodiscord/Message.java
+++ src/main/java/xyz/earthcow/themistodiscord/Message.java
@@ -12,7 +12,6 @@ import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -43,21 +42,7 @@ public class Message {
         this.message = message;
         this.name = Objects.requireNonNull(message.getNameAsString());
 
-        // Discover the webhook url to be used
-        String localWebhookUrl;
-        if (message.getBoolean("CustomWebhook.Enabled", false)) {
-            localWebhookUrl = message.getString("CustomWebhook.Url", "");
-            if (Utils.isInvalidWebhookUrl(localWebhookUrl)) {
-                if (!localWebhookUrl.isEmpty()) {
-                    ttd.log(LogLevel.WARN,  "Invalid custom webhook url for message: " + name + "! This message will use the global webhook url.");
-                }
-                localWebhookUrl = message.getRoot().getString("webhookUrl");
-            }
-        } else {
-            localWebhookUrl = message.getRoot().getString("webhookUrl");
-        }
-        // Set the webhook url for this message
-        this.webhookUrl = localWebhookUrl;
+        this.webhookUrl = determineWebhookUrl();
 
         // Determine and set the webhook json
         String jsonString = message.getString("Json", "");
@@ -74,6 +59,22 @@ public class Message {
         } else {
             this.handling = null;
         }
+    }
+
+    private String determineWebhookUrl() {
+        String localWebhookUrl;
+        if (message.getBoolean("CustomWebhook.Enabled", false)) {
+            localWebhookUrl = message.getString("CustomWebhook.Url", "");
+            if (Utils.isInvalidWebhookUrl(localWebhookUrl)) {
+                if (!localWebhookUrl.isEmpty()) {
+                    ttd.log(LogLevel.WARN,  "Invalid custom webhook url for message: " + name + "! This message will use the global webhook url.");
+                }
+                localWebhookUrl = message.getRoot().getString("webhookUrl");
+            }
+        } else {
+            localWebhookUrl = message.getRoot().getString("webhookUrl");
+        }
+        return localWebhookUrl;
     }
 
     private String getJsonWebhook() {
@@ -93,87 +94,37 @@ public class Message {
             return webhook.getJsonString();
         }
 
+        webhook.addEmbed(getEmbedFromConfig(embedSection));
+        return webhook.getJsonString();
+    }
+
+    private DiscordWebhook.EmbedObject getEmbedFromConfig(@NotNull Section embedSection) {
         // Define the embed object
         DiscordWebhook.EmbedObject embed = new DiscordWebhook.EmbedObject();
 
         // Set the color
-        String colorStr = embedSection.getString("Color", "");
-        if (colorStr.contains("%category_color%")) {
-            embed.setColorStr(colorStr);
-        } else {
-            try {
-                embed.setColor(
-                    Color.decode(colorStr)
-                );
-            } catch (NumberFormatException e) {
-                ttd.log(LogLevel.WARN, "Invalid color string: " + colorStr +  " for message: " + name + ". Using black.");
-                ttd.log(LogLevel.DEBUG, "Exception: " + e);
-                embed.setColor(Color.BLACK);
-            }
-        }
-
+        EmbedUtil.applyColor(embed, embedSection.getString("Color", ""), ttd, name);
         // Set the author
-        if (embedSection.get("Author", null) != null) {
-            embed.setAuthor(
-                embedSection.getString("Author.Name"),
-                embedSection.getString("Author.Url"),
-                embedSection.getString("Author.ImageUrl")
-            );
-        }
-
+        EmbedUtil.applyAuthor(embed, embedSection.getSection("Author"));
         // Set the thumbnail url
-        embed.setThumbnail(embedSection.getString("ThumbnailUrl", null));
-
+        embed.setThumbnail(embedSection.getString("ThumbnailUrl"));
         // Set the title
-        if (embedSection.get("Title", null) != null) {
-            embed.setTitle(embedSection.getString("Title.Text", null));
-            embed.setUrl(embedSection.getString("Title.Url", null));
-        }
-
+        EmbedUtil.applyTitle(embed, embedSection.getSection("Title"));
         // Set the description
-        embed.setDescription(embedSection.getString("Description", null));
-
+        embed.setDescription(embedSection.getString("Description"));
         // Set the fields
-        List<String> fields = embedSection.getStringList("Fields");
-        if (!fields.isEmpty()) {
-
-            for (String field : fields) {
-                if (field.contains(";")) {
-
-                    String[] parts = field.split(";");
-                    if (parts.length < 2) {
-                        continue;
-                    }
-
-                    boolean inline = parts.length < 3 || Boolean.parseBoolean(parts[2]);
-
-                    embed.addField(parts[0], parts[1], inline);
-                } else {
-                    boolean inline = Boolean.parseBoolean(field);
-                    embed.addField("\u200e", "\u200e", inline);
-                }
-            }
-
-        }
-
+        EmbedUtil.applyFields(embed, embedSection.getStringList("Fields"));
         // Set the image url
-        embed.setImage(embedSection.getString("ImageUrl", null));
-
+        embed.setImage(embedSection.getString("ImageUrl"));
         // Set the footer
-        if (embedSection.get("Footer", null) != null) {
-            embed.setFooter(
-                embedSection.getString("Footer.Text", null),
-                embedSection.getString("Footer.IconUrl", null)
-            );
-        }
+        EmbedUtil.applyFooter(embed, embedSection.getSection("Footer"));
 
         // Set the timestamp
         if (embedSection.getBoolean("Timestamp")) {
             embed.setTimestamp((new Date()).toInstant());
         }
 
-        webhook.addEmbed(embed);
-        return webhook.getJsonString();
+        return embed;
     }
 
     public void execute(@NotNull Player player, @NotNull String detectionType, double score, double ping, double tps, @Nullable CommandSender sender) {
@@ -189,37 +140,7 @@ public class Message {
                 if (e instanceof FileNotFoundException) {
                     msg = "Your webhook url is not valid! Update it with /ttd url <url>!";
                 } else {
-                    String message = e.getMessage();
-                    if (message != null && message.contains("HTTP response code:")) {
-                        try {
-                            int responseCode = Integer.parseInt(message.substring(message.indexOf(":") + 2, message.indexOf(":") + 5));
-                            switch (responseCode) {
-                                case 400:
-                                    msg = "Error - 400 response - bad request. Verify all urls are either blank or valid urls.";
-                                    break;
-                                case 401:
-                                    msg = "Error - 401 response - unauthorized. Verify webhook url and discord server status.";
-                                    break;
-                                case 403:
-                                    msg = "Error - 403 response - forbidden. Verify webhook url and discord server status.";
-                                    break;
-                                case 404:
-                                    msg = "Error - 404 response - not found. Verify webhook url and discord server status.";
-                                    break;
-                                case 429:
-                                    msg = "Error - 429 response - too many requests. This webhook has sent too many messages in too short amount of time.";
-                                    break;
-                                case 500:
-                                    msg = "Error - 505 response - internal server error. Discord services may be temporarily down.";
-                                    break;
-                                default:
-                                    msg = "Error - " + responseCode + " response - unexpected error code.";
-                                    break;
-                            }
-                        } catch (IndexOutOfBoundsException | NumberFormatException ex) {
-                            ttd.log(LogLevel.DEBUG, "Secondary exception: " + ex);
-                        }
-                    }
+                    msg = getHttpErrorMsg(e.getMessage());
                 }
                 if (msg == null) {
                     msg = "Unknown error has occurred. Please make a bug report at https://github.com/RagingTech/ThemisToDiscord/issues.";
@@ -233,6 +154,34 @@ public class Message {
                 ttd.log(LogLevel.DEBUG, "Webhook: " + webhookJson);
             }
         });
+    }
+
+    @Nullable
+    private String getHttpErrorMsg(@Nullable String message) {
+        String msg = null;
+        if (message != null && message.contains("HTTP response code:")) {
+            try {
+                int responseCode = Integer.parseInt(message.substring(message.indexOf(":") + 2, message.indexOf(":") + 5));
+                msg = switch (responseCode) {
+                    case 400 ->
+                            "Error - 400 response - bad request. Verify all urls are either blank or valid urls.";
+                    case 401 ->
+                            "Error - 401 response - unauthorized. Verify webhook url and discord server status.";
+                    case 403 ->
+                            "Error - 403 response - forbidden. Verify webhook url and discord server status.";
+                    case 404 ->
+                            "Error - 404 response - not found. Verify webhook url and discord server status.";
+                    case 429 ->
+                            "Error - 429 response - too many requests. This webhook has sent too many messages in too short amount of time.";
+                    case 500 ->
+                            "Error - 505 response - internal server error. Discord services may be temporarily down.";
+                    default -> "Error - " + responseCode + " response - unexpected error code.";
+                };
+            } catch (IndexOutOfBoundsException | NumberFormatException ex) {
+                ttd.log(LogLevel.DEBUG, "Secondary exception: " + ex);
+            }
+        }
+        return msg;
     }
 
     public @NotNull String getName() {
